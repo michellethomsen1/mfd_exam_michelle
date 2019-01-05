fb62f73e5562cfe1afbd44f281caebbe1cac985d
Michelle Thomsen <mich6153@edu.eal.dk>
2019-01-05 22:32:49 +0100

    Header

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,11 +1,34 @@
 <template>
     <div id="header">
         <header>
-            <h1>hej header</h1>
-            <div class="logo">
-
-            </div>
+            <router-link to="/">
+                <img src="../assets/images/surfhouse-logo.png" alt="surfhouse logo">
+            </router-link>
             
+            <div class="basket-container">
+                <div class="login">
+                    <button>LOG IN</button>
+                    <button>WISH LIST(0)</button>
+                </div>
+                
+                <div class="basket">
+                    <img src="../assets/images/icon-cart.png" alt="basket">
+                    <div class="detail">
+                        <p class="price">€1,459.50</p>
+                        <p class="items">2 items</p>
+                    </div>
+
+                    <button class="btn-view">VIEW CART</button>
+                    <button class="btn-checkout">CHECKOUT</button>
+                </div>
+
+                <div class="search">
+                    <form>
+                        <input type="text" placeholder="SEARCH" name="search">
+                        <button type="submit"><i class="fa fa-search"></i></button>
+                    </form>
+                </div>
+            </div>
         </header>
     </div>
 </template>
