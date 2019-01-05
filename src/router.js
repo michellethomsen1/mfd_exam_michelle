8d48dda5a35fbbba1173436350dc5cda2e07fdf1
Michelle Thomsen <mich6153@edu.eal.dk>
2019-01-05 12:09:46 +0100

    OpsÃtning af views

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Product from './views/Product.vue'
+import Contact from './views/Contact.vue'
 
 Vue.use(Router)
 
@@ -13,13 +15,25 @@ export default new Router({
       name: 'home',
       component: Home
     },
+
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/product',
+      name: 'product',
+      component: Product
+    },
+
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact
     }
+    //{
+    //  path: '/about',
+    //  name: 'about',
+    //  // route level code-splitting
+    //  // this generates a separate chunk (about.[hash].js) for this route
+    //  // which is lazy-loaded when the route is visited.
+    //  component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    //}
   ]
 })
