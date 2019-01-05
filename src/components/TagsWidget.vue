a1d61e8c3e22251901862208dfa63dcc3c8b338e
Michelle Thomsen <mich6153@edu.eal.dk>
2019-01-06 00:20:37 +0100

    Tags section added

--- src/components/TagsWidget.vue
+++ src/components/TagsWidget.vue
@@ -1,6 +1,29 @@
 <template>
     <div id="tags">
-        <h1>her kommer tags</h1>
+        <h2>Tags Widget</h2>
+
+        <div class="singletag">
+            <button>Kitesurf</button>
+            <button>Super</button>
+            <button>Duper</button>
+            <button>Theme</button>
+            <button>Men</button>
+            <button>Women</button>
+            <button>Equipment</button>
+            <button>Best</button>
+            <button>Accessories</button>
+            <button>Men</button>
+            <button>Apparel</button>
+            <button>Super</button>
+            <button>Duper</button>
+            <button>Theme</button>
+            <button>Responsiv</button>
+            <button>Men</button>
+            <button>Women</button>
+            <button>Equipment</button>
+        </div>
+
+        <a href="#">View all tags <i class="fas fa-long-arrow-alt-right"></i></a>
     </div>
 </template>
 
