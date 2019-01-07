5514feb1cceb820056d347810c274ea3e6bac130
Michelle Thomsen <mich6153@edu.eal.dk>
2019-01-07 16:00:19 +0100

    product section load from json

--- src/components/ProductSection.vue
+++ src/components/ProductSection.vue
@@ -8,11 +8,44 @@
                 <button><i class="fas fa-angle-right"></i></button>
             </div>
         </div>
+
+        <div class="products">
+            <div class="product-single" v-for="product in products" :key="product.id">
+                <picture>
+                    <router-link :to="`/product/${product.id}`"><img :src="product.productImg"></router-link>
+                </picture>
+
+                <div class="product-info">
+                    <router-link :to="`/product/${product.id}`"><h3>{{product.productTitle}}</h3></router-link>
+
+                    <div class="price">
+                        <router-link :to="`/product/${product.id}`"><p>{{product.productPrice}}</p></router-link>
+                        <p class="price-before">{{product.productPriceBefore}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import axios from 'axios'
     export default {
         name: 'ProductSection',
+        data () {
+            return {
+            products: null
+            }
+        },
+        mounted () {
+            this.getData()
+        },
+        methods: {
+            getData () {
+            axios
+                .get('/listProducts.json')
+                .then(response => (this.products = response.data.products))
+            }
+        }
     }
 </script>
--- src/router.js
+++ src/router.js
@@ -15,25 +15,15 @@ export default new Router({
       name: 'home',
       component: Home
     },
-
     {
       path: '/product',
       name: 'product',
       component: Product
     },
-
     {
       path: '/contact',
       name: 'contact',
       component: Contact
     }
-    //{
-    //  path: '/about',
-    //  name: 'about',
-    //  // route level code-splitting
-    //  // this generates a separate chunk (about.[hash].js) for this route
-    //  // which is lazy-loaded when the route is visited.
-    //  component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    //}
   ]
 })
